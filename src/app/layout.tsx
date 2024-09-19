3352ad1b723efe791679497e6118f1304ce5b216
Iceship <iceship@gmail.com>
2024-09-19 17:33:00 +0900

    add nextui

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,7 @@
 import type { Metadata } from "next";
-import { Suspense } from "react";
+
+import AppNavBar from "@/components/app-navbar";
+import Providers from "@/components/providers";
 
 import "./globals.css";
 
@@ -21,8 +23,13 @@ export default function RootLayout({
           href="data:image/svg+xml,<svg xmlns=%22http://www.w3.org/2000/svg%22 viewBox=%220 0 100 100%22><text y=%22.9em%22 font-size=%2290%22>📦</text></svg>"
         />
       </head>
-      <body>
-        <Suspense>{children}</Suspense>
+      <body className="h-screen w-screen">
+        <Providers>
+          <AppNavBar />
+          <main className="flex-grow overflow-auto bg-[url(/light-bg.svg)] bg-cover bg-repeat dark:bg-[url(/dark-bg.svg)]">
+            {children}
+          </main>
+        </Providers>
       </body>
     </html>
   );
